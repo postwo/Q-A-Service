76d3b22e786e603103f48c82672f3eace308714b
postwo <codo7717@naver.com>
2024-03-29 14:22:35 +0900

    세팅

---
+++ src/test/java/org/sbs/exam/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sbs.exam.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
